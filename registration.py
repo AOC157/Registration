f13b46d85eb7e6a7750bbf2ec18983ea55eac63e
Hatef Zamany <hatef.zamany@yahoo.com>
2020-08-22 04:56:22 -0700

    just added some spaces and signs

--- registration.py
+++ registration.py
@@ -2,14 +2,14 @@ class SignIn:
     def __init__(self,username,password):
         self.username = username
         self.password = password
-        print(username + " " + password + " in")
+        print(username + " " + password + " in!")
 
 
 class SignUp:
     def __init__(self,username,password):
         self.username = username
         self.password = password
-        print(username + " " + password + " up")
+        print(username + " " + password + " up!")
 
     def save(self,username,password):
         usernameFile = open("username.txt","a")
@@ -23,8 +23,8 @@ class SignUp:
 
         
 mode = input("enter mode: sign ")
-username = input("Please enter your username")
-password = input("Please enter your password")
+username = input("Please enter your username: ")
+password = input("Please enter your password: ")
 
 if mode == "in":
     user = SignIn(username,password)
