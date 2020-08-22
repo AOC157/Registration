f628af9925fe0066cd0d6233444cf43892a0c30c
Hatef Zamany <hatef.zamany@yahoo.com>
2020-08-22 09:48:46 -0700

    load function implemented

--- registration.py
+++ registration.py
@@ -4,6 +4,34 @@ class SignIn:
         self.password = password
         print(username + " " + password + " in!")
 
+    def load(self):
+        userCounter = 0
+        usernameFile = open("username.txt","r")
+        passwordFile = open("password.txt","r")
+        username = usernameFile.readline()
+        while(username != self.username + '\n'):
+            userCounter += 1
+            username = usernameFile.readline()
+            if(username == ""):
+                usernameFile.close()
+                passwordFile.close()
+                return False
+        passCounter = 0
+        while (passCounter < userCounter):
+            passwordFile.readline()
+            passCounter += 1
+        password = passwordFile.readline()
+        if (password == self.password + '\n'):
+            usernameFile.close()
+            passwordFile.close()
+            return True
+
+        else:
+            print(password)
+            usernameFile.close()
+            passwordFile.close()
+            return False
+
 
 class SignUp:
     def __init__(self,username,password):
@@ -28,8 +56,13 @@ password = input("Please enter your password: ")
 
 if mode == "in":
     user = SignIn(username,password)
+    if user.load():
+        print("you succesfully signed in \"" + username + "\"")
+    else:
+        print("your password may be incorrect or your username doesn't exist")
     
 elif mode == "up":
     user = SignUp(username,password)
     user.save(username,password)
+    print("\"" + username + "\"! you succesfully signed up")
 
